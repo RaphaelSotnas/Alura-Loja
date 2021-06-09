654f94420e5147abfa0f4dd45fcc4aafa450b123
RaphaelSotnas <raphael.sants@hotmail.com>
2021-06-09 19:29:57 -0300

    Meu primeiro projeto de aprendizado da Alura no C#

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class ProdutoContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AppDB;Trusted_Connection=true;");
+        }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GravarUsandoAdoNet();
+            //PegarProdutoPorId(1);
+            //EditarProduto(2);
+            DeletarProduto(1);
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Pedra Filosofal";
+            p.Categoria = "DVD";
+            p.Preco = 58.887;
+
+            ProdutoContext produtoContext = new ProdutoContext();
+
+            produtoContext.Produtos.Add(p);
+            produtoContext.SaveChanges();
+            
+        }
+
+        private static Produto PegarProdutoPorId(int id)
+        {
+            ProdutoContext produtoContext = new ProdutoContext();
+
+            Produto produto = produtoContext.Produtos.FirstOrDefault(p => p.Id == id);
+            Console.WriteLine(produto.Nome);
+            Console.ReadLine();
+            return produto;
+        }
+        private static void EditarProduto(int id)
+        {
+            ProdutoContext produtoContext = new ProdutoContext();
+
+            var produto = produtoContext.Produtos.FirstOrDefault(p => p.Id == id);
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Categoria);
+            Console.WriteLine("/////////--------//////////");
+            produto.Categoria = "Livro";
+
+            produtoContext.Produtos.Update(produto);
+            produtoContext.SaveChanges();
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Categoria);
+            Console.ReadLine();
+        }
+        public static void DeletarProduto(int id)
+        {
+            ProdutoContext produtoContext = new ProdutoContext();
+
+            var produtoRecuperadoDoBanco = PegarProdutoPorId(id);
+
+            produtoContext.Produtos.Remove(produtoRecuperadoDoBanco);
+            produtoContext.SaveChanges();
+        }
+
+
+    }
+}
